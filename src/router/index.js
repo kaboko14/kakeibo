80e6640dfc77f7223d80ff72051d9c6382036f37
Kahori <aokitori6211@yahoo.co.jp>
2020-09-25 13:00:35 +0900

    入力画面のvue-router遷移時はスクロールしない

--- src/router/index.js
+++ src/router/index.js
@@ -87,8 +87,8 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior() {
-    return { x: 0, y: 0 };
+  scrollBehavior(to) {
+    if (to.name !== 'AddExpense' && to.name !== 'AddIncome') { return { x: 0, y: 0 }; }
   }
 });
 
